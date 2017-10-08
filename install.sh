70ae4830dc8b9237780ba950bfbb94329ee8c9ad
gamedll <gamedll@163.com>
2017-10-09 00:22:39 +0800

    Update install.sh
    remove:yum groupinstall -y "Development tools"

--- install.sh
+++ install.sh
@@ -3,14 +3,9 @@
 #Git主页: https://github.com/LunacyZeus
 
 echo "Python3.6 for Centos 7.0 一键安装脚本\nGitHub主页地址: https://github.com/LunacyZeus\n这个脚本是我在经过多次测试后做出来的,安装了Python要用的所有的必需库(如有缺少,请指出,谢谢!),不会出现在使用中需要某些库而重新编译安装Python的蛋疼事情\n\nPs: 本脚本在腾讯云亲测可用\n  遇到问题请在Github提交BUG"
-
-#read -p "请按任意键以继续" var
-
-
 echo "Let's Go..."
-
 echo "安装开发环境工具..."
-yum groupinstall -y "Development tools"
+# yum groupinstall -y "Development tools"
 
 echo "安装编译Python必需工具..."
 yum install -y openssl-devel bzip2-devel expat-devel gdbm-devel readline-devel sqlite-devel
